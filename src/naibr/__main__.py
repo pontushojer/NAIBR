349c45ce69c2b85ef1586ba477c62f4a0a22694a
Pontus Höjer <pontushojer@gmail.com>
2022-07-24 12:54:10 +0200

    Run black

--- src/naibr/__main__.py
+++ src/naibr/__main__.py
@@ -1,8 +1,8 @@
-from __future__ import print_function,division
-import os,sys,time,json,pysam,collections
+from __future__ import print_function, division
+import os, sys, time, json, pysam, collections
 
 if len(sys.argv) < 2:
-	raise ValueError('No input config file')
+    raise ValueError("No input config file")
 import multiprocessing as mp
 from .get_reads import *
 from .global_vars import *
@@ -12,78 +12,123 @@ from .rank import *
 
 
 def run_NAIBR_user(cand):
-	'''
-	use user input candidate novel adjacencies
-	'''
-	scores = 0
-	reads_by_LR,LRs_by_pos,discs_by_barcode,cands,coverage = make_barcodeDict_user(cand)
-	p_len,p_rate,overlap = get_distributions(reads_by_LR)
-	if p_len == None:
-		return scores
-	scores = predict_NAs(reads_by_LR,LRs_by_pos,discs_by_barcode,cands,p_len,p_rate,coverage,False)
-	return scores
+    """
+    use user input candidate novel adjacencies
+    """
+    scores = 0
+    reads_by_LR, LRs_by_pos, discs_by_barcode, cands, coverage = make_barcodeDict_user(
+        cand
+    )
+    p_len, p_rate, overlap = get_distributions(reads_by_LR)
+    if p_len == None:
+        return scores
+    scores = predict_NAs(
+        reads_by_LR, LRs_by_pos, discs_by_barcode, cands, p_len, p_rate, coverage, False
+    )
+    return scores
 
 
 def run_NAIBR(chrom):
-	'''
-	automatically identify candidate novel adjacencies
-	'''
-	reads_by_LR,LRs_by_pos,discs_by_barcode,discs,interchrom_discs,coverage = make_barcodeDict(chrom)
-	scores = 0
-	if len(reads_by_LR) > 0:
-		cands,p_len,p_rate = get_candidates(discs,reads_by_LR)
-		if cands == None:
-			print('No candidates from %s'%chrom)
-			return reads_by_LR,LRs_by_pos,discs_by_barcode,interchrom_discs,coverage,scores
-		print('ranking %i candidates from %s'%(len(cands),chrom))
-		scores = predict_NAs(reads_by_LR,LRs_by_pos,discs_by_barcode,cands,p_len,p_rate,coverage,False)
-	else:
-		print('No candidates from %s'%chrom)
-	return reads_by_LR,LRs_by_pos,discs_by_barcode,interchrom_discs,coverage,scores
+    """
+    automatically identify candidate novel adjacencies
+    """
+    (
+        reads_by_LR,
+        LRs_by_pos,
+        discs_by_barcode,
+        discs,
+        interchrom_discs,
+        coverage,
+    ) = make_barcodeDict(chrom)
+    scores = 0
+    if len(reads_by_LR) > 0:
+        cands, p_len, p_rate = get_candidates(discs, reads_by_LR)
+        if cands == None:
+            print("No candidates from %s" % chrom)
+            return (
+                reads_by_LR,
+                LRs_by_pos,
+                discs_by_barcode,
+                interchrom_discs,
+                coverage,
+                scores,
+            )
+        print("ranking %i candidates from %s" % (len(cands), chrom))
+        scores = predict_NAs(
+            reads_by_LR,
+            LRs_by_pos,
+            discs_by_barcode,
+            cands,
+            p_len,
+            p_rate,
+            coverage,
+            False,
+        )
+    else:
+        print("No candidates from %s" % chrom)
+    return reads_by_LR, LRs_by_pos, discs_by_barcode, interchrom_discs, coverage, scores
+
 
 def main():
-	starttime = time.time()
-	if len(candidates) > 0:
-		print('user defined candidates')
-		with open(candidates) as f:
-			cands = f.read().split('\n')
-			cands = [x.split('\t') for x in cands]
-			cands = [x for x in cands if x]
-			cands = [[x[0],int(x[1]),x[3],int(x[4]),x[-1]] for x in cands if len(x) >= 4]
-		scores = flatten(parallel_execute(run_NAIBR_user,cands))
-		write_scores(scores)
+    starttime = time.time()
+    if len(candidates) > 0:
+        print("user defined candidates")
+        with open(candidates) as f:
+            cands = f.read().split("\n")
+            cands = [x.split("\t") for x in cands]
+            cands = [x for x in cands if x]
+            cands = [
+                [x[0], int(x[1]), x[3], int(x[4]), x[-1]] for x in cands if len(x) >= 4
+            ]
+        scores = flatten(parallel_execute(run_NAIBR_user, cands))
+        write_scores(scores)
 
-	else:
-		reads = pysam.AlignmentFile(BAM_FILE,"rb")
-		chroms = reads.references
-		chroms = [x for x in chroms if is_proper_chrom(x)]
-		data = parallel_execute(run_NAIBR,chroms)
-		reads_by_LR = collections.defaultdict(list)
-		LRs_by_pos = collections.defaultdict(list)
-		discs_by_barcode = collections.defaultdict(list)
-		discs = collections.defaultdict(list)
-		coverage = []
-		scores = []
-		for reads_by_LR_chrom,LRs_by_pos_chrom,discs_by_barcode_chrom,discs_chrom,cov_chrom,scores_chrom in data:
-			if scores_chrom:
-				reads_by_LR.update(reads_by_LR_chrom)
-				LRs_by_pos.update(LRs_by_pos_chrom)
-				discs_by_barcode.update(discs_by_barcode_chrom)
-				discs.update(discs_chrom)
-				coverage.append(cov_chrom)
-				scores += scores_chrom
-		cands,p_len,p_rate = get_candidates(discs,reads_by_LR)
-		if cands != None:
-			print('ranking %i interchromosomal candidates'%len(cands))
-			scores += predict_NAs(reads_by_LR,LRs_by_pos,discs_by_barcode,cands,p_len,p_rate,np.mean(coverage),True)
-		else:
-			print('No interchromosomal candidates')
-		write_scores(scores)
+    else:
+        reads = pysam.AlignmentFile(BAM_FILE, "rb")
+        chroms = reads.references
+        chroms = [x for x in chroms if is_proper_chrom(x)]
+        data = parallel_execute(run_NAIBR, chroms)
+        reads_by_LR = collections.defaultdict(list)
+        LRs_by_pos = collections.defaultdict(list)
+        discs_by_barcode = collections.defaultdict(list)
+        discs = collections.defaultdict(list)
+        coverage = []
+        scores = []
+        for (
+            reads_by_LR_chrom,
+            LRs_by_pos_chrom,
+            discs_by_barcode_chrom,
+            discs_chrom,
+            cov_chrom,
+            scores_chrom,
+        ) in data:
+            if scores_chrom:
+                reads_by_LR.update(reads_by_LR_chrom)
+                LRs_by_pos.update(LRs_by_pos_chrom)
+                discs_by_barcode.update(discs_by_barcode_chrom)
+                discs.update(discs_chrom)
+                coverage.append(cov_chrom)
+                scores += scores_chrom
+        cands, p_len, p_rate = get_candidates(discs, reads_by_LR)
+        if cands != None:
+            print("ranking %i interchromosomal candidates" % len(cands))
+            scores += predict_NAs(
+                reads_by_LR,
+                LRs_by_pos,
+                discs_by_barcode,
+                cands,
+                p_len,
+                p_rate,
+                np.mean(coverage),
+                True,
+            )
+        else:
+            print("No interchromosomal candidates")
+        write_scores(scores)
 
+    print("Finished in", (time.time() - starttime) / 60.0, "minutes")
+    return
 
-
-	print('Finished in',(time.time()-starttime)/60.0,'minutes')
-	return
 
 if __name__ == "__main__":
     main()
--- src/naibr/distributions.py
+++ src/naibr/distributions.py
@@ -1,11 +1,12 @@
-from __future__ import print_function,division
+from __future__ import print_function, division
 from future.utils import iteritems
-import os,sys,pysam,collections,time,gc,random,itertools
+import os, sys, pysam, collections, time, gc, random, itertools
 from .utils import *
 import numpy as np
 from .global_vars import *
 import matplotlib as mpl
-mpl.use('Agg')
+
+mpl.use("Agg")
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 from pylab import rc
@@ -18,133 +19,150 @@ import mpmath
 
 
 class negBin(object):
-	def __init__(self, p = 0.1, r = 10):
-		nbin_mpmath = lambda k, p, r: mpmath.gamma(k + r)/(mpmath.gamma(k+1)\
-			*mpmath.gamma(r))*np.power(1-p, r)*np.power(p, k)
-		self.nbin = np.frompyfunc(nbin_mpmath, 3, 1)
-		self.p = p
-		self.r = r
- 
-	def mleFun(self, par, data, sm):
-		p = par[0]
-		r = par[1]
-		n = len(data)
-		f0 = sm/(r+sm)-p
-		f1 = np.sum(special.psi(data+r)) - n*special.psi(r) + n*np.log(r/(r+sm))
-		return np.array([f0, f1])
- 
-	def fit(self, data, p = None, r = None):
-		if p is None or r is None:
-			av = np.average(data)
-			va = np.var(data)
-			r = (av*av)/(va-av)
-			p = (va-av)/(va)
-		sm = np.sum(data)/len(data)
-		x = optimize.fsolve(self.mleFun, np.array([p, r]), args=(data, sm))
-		self.p = x[0]
-		self.r = x[1]
- 
-	def pdf(self, k):
-		return self.nbin(k, self.p, self.r).astype('float64')
+    def __init__(self, p=0.1, r=10):
+        nbin_mpmath = (
+            lambda k, p, r: mpmath.gamma(k + r)
+            / (mpmath.gamma(k + 1) * mpmath.gamma(r))
+            * np.power(1 - p, r)
+            * np.power(p, k)
+        )
+        self.nbin = np.frompyfunc(nbin_mpmath, 3, 1)
+        self.p = p
+        self.r = r
+
+    def mleFun(self, par, data, sm):
+        p = par[0]
+        r = par[1]
+        n = len(data)
+        f0 = sm / (r + sm) - p
+        f1 = (
+            np.sum(special.psi(data + r))
+            - n * special.psi(r)
+            + n * np.log(r / (r + sm))
+        )
+        return np.array([f0, f1])
+
+    def fit(self, data, p=None, r=None):
+        if p is None or r is None:
+            av = np.average(data)
+            va = np.var(data)
+            r = (av * av) / (va - av)
+            p = (va - av) / (va)
+        sm = np.sum(data) / len(data)
+        x = optimize.fsolve(self.mleFun, np.array([p, r]), args=(data, sm))
+        self.p = x[0]
+        self.r = x[1]
+
+    def pdf(self, k):
+        return self.nbin(k, self.p, self.r).astype("float64")
 
 
+def plot_distribution(p, distr, xlab, ylab, title):
+    fname = "_".join(title.split(" "))
+    nbins = 50
+    fig, ax = plt.subplots()
+    n, bins, patches = plt.hist(distr, nbins, normed=True, facecolor="blue", alpha=0.70)
+    rc("axes", linewidth=1)
+    y = [p(b) for b in bins]
+    plt.plot(bins, y, color="r", linewidth=5)
+    plt.xlabel(xlab)
+    plt.ylabel(ylab)
+    plt.title(title)
+    x0 = max(n)
+    xmax = max(bins)
+    plt.axis([0, max(bins), 0, max(max(y), max(n))])
+    fig.savefig(os.path.join(DIR, fname + ".pdf"), format="pdf")
+    plt.close("all")
+    return
 
-def plot_distribution(p,distr,xlab,ylab,title):
-	fname = '_'.join(title.split(' '))
-	nbins = 50
-	fig, ax = plt.subplots()
-	n, bins, patches = plt.hist(distr,nbins,normed=True, facecolor='blue', alpha=0.70)
-	rc('axes', linewidth=1)
-	y = [p(b) for b in bins]
-	plt.plot(bins,y,color='r',linewidth=5)
-	plt.xlabel(xlab)
-	plt.ylabel(ylab)
-	plt.title(title)
-	x0 = max(n)
-	xmax = max(bins)
-	plt.axis([0, max(bins), 0, max(max(y),max(n))])
-	fig.savefig(os.path.join(DIR,fname+'.pdf'),format='pdf')
-	plt.close('all')
-	return
 
-def linked_reads(reads,chrom):
-	reads.sort(key = lambda x: x[0])
+def linked_reads(reads, chrom):
+    reads.sort(key=lambda x: x[0])
 
-	curr_LR = [0,0,0,0]
-	LRs = []
-	for start,end,hap,mapq in reads:
-		if curr_LR[0] == 0 or start-curr_LR[2] > d:
-			if curr_LR[0] != 0 and curr_LR[3] >= min_reads and curr_LR[2]-curr_LR[1] >= min_len:
-				LRs.append(curr_LR)
-			curr_LR = [chrom,start,end,1]
-		else:
-			curr_LR[2] = max(curr_LR[2],end)
-			curr_LR[3] += 1
-	if curr_LR[0] != 0 and curr_LR[3] >= min_reads and curr_LR[2]-curr_LR[1] >= min_len:
-		LRs.append(curr_LR)
-	return LRs
+    curr_LR = [0, 0, 0, 0]
+    LRs = []
+    for start, end, hap, mapq in reads:
+        if curr_LR[0] == 0 or start - curr_LR[2] > d:
+            if (
+                curr_LR[0] != 0
+                and curr_LR[3] >= min_reads
+                and curr_LR[2] - curr_LR[1] >= min_len
+            ):
+                LRs.append(curr_LR)
+            curr_LR = [chrom, start, end, 1]
+        else:
+            curr_LR[2] = max(curr_LR[2], end)
+            curr_LR[3] += 1
+    if (
+        curr_LR[0] != 0
+        and curr_LR[3] >= min_reads
+        and curr_LR[2] - curr_LR[1] >= min_len
+    ):
+        LRs.append(curr_LR)
+    return LRs
 
 
 def get_overlap(barcode_LRs):
-	global barcode_overlap
-	for LR1,LR2 in itertools.combinations(barcode_LRs,2):
-		if LR1[0] > LR2[0] or LR1[1] > LR2[1]:
-			LR1,LR2 = [LR2,LR1]
-		chr1,start1,end1,count1 = LR1
-		chr2,start2,end2,count2 = LR2
-		index1 = set([int(start1/d)*d,int(end1/d)*d])
-		index2 = set([int(start2/d)*d,int(end2/d)*d])
-		for id1 in index1:
-			for id2 in index2:
-				barcode_overlap[(chr1,id1,chr2,id2)] += 1
-
+    global barcode_overlap
+    for LR1, LR2 in itertools.combinations(barcode_LRs, 2):
+        if LR1[0] > LR2[0] or LR1[1] > LR2[1]:
+            LR1, LR2 = [LR2, LR1]
+        chr1, start1, end1, count1 = LR1
+        chr2, start2, end2, count2 = LR2
+        index1 = set([int(start1 / d) * d, int(end1 / d) * d])
+        index2 = set([int(start2 / d) * d, int(end2 / d) * d])
+        for id1 in index1:
+            for id2 in index2:
+                barcode_overlap[(chr1, id1, chr2, id2)] += 1
 
 
 def get_distributions(reads_by_LR):
-	LRs = []
-	global barcode_overlap,LRs_by_barcode
-	LRs_by_barcode = collections.defaultdict(list)
-	barcode_overlap = collections.defaultdict(int)
-	for key,reads in iteritems(reads_by_LR):
-		chrom,barcode = key
-		barcode_LRS = linked_reads(reads,chrom)
-		LRs += barcode_LRS
-		LRs_by_barcode[barcode] += barcode_LRS
-	for barcode,barcode_LRS in iteritems(LRs_by_barcode):
-		if len(barcode_LRS) > 1:
-			get_overlap(barcode_LRS)
-	if len(LRs) < 100:
-		return None,None,None
-	p_rate = get_rate_distr(LRs)
-	p_len = get_length_distr(LRs)	
-	return p_len,p_rate,barcode_overlap
+    LRs = []
+    global barcode_overlap, LRs_by_barcode
+    LRs_by_barcode = collections.defaultdict(list)
+    barcode_overlap = collections.defaultdict(int)
+    for key, reads in iteritems(reads_by_LR):
+        chrom, barcode = key
+        barcode_LRS = linked_reads(reads, chrom)
+        LRs += barcode_LRS
+        LRs_by_barcode[barcode] += barcode_LRS
+    for barcode, barcode_LRS in iteritems(LRs_by_barcode):
+        if len(barcode_LRS) > 1:
+            get_overlap(barcode_LRS)
+    if len(LRs) < 100:
+        return None, None, None
+    p_rate = get_rate_distr(LRs)
+    p_len = get_length_distr(LRs)
+    return p_len, p_rate, barcode_overlap
+
 
 def get_length_distr(LRs):
-	lengths = [x[2]-x[1] for x in LRs]
-	lengths.sort()
-	assert len(lengths) >= 100
-	assert np.var(lengths) != 0
-	b = negBin()
-	b.fit(lengths)
-	p = b.pdf
-	pp = lambda x: max(1e-20,float(p([x])[0]))
-	## poisson distribution
-	# p = scipy.stats.poisson(np.mean(lengths)).pmf
-	# pp = lambda x: max(1e-20,float(p([x])[0]))
-	#plot_distribution(pp,lengths,'Linked-read lengths (bp)','Frequency','Linked-read length distribution')
-	return pp
+    lengths = [x[2] - x[1] for x in LRs]
+    lengths.sort()
+    assert len(lengths) >= 100
+    assert np.var(lengths) != 0
+    b = negBin()
+    b.fit(lengths)
+    p = b.pdf
+    pp = lambda x: max(1e-20, float(p([x])[0]))
+    ## poisson distribution
+    # p = scipy.stats.poisson(np.mean(lengths)).pmf
+    # pp = lambda x: max(1e-20,float(p([x])[0]))
+    # plot_distribution(pp,lengths,'Linked-read lengths (bp)','Frequency','Linked-read length distribution')
+    return pp
+
 
 def get_rate_distr(LRs):
-	rate = [x[3]/float(x[2]-x[1]) for x in LRs]
-	rate.sort()
-	if len(rate) > 10:
-		rate = rate[int(len(rate)/10):int(len(rate)/10*9)]
-	alpha,loc,beta = scipy.stats.gamma.fit(rate)
-	p = scipy.stats.gamma(alpha,loc,beta).cdf
-	pp = lambda x: max(1e-20,float(p([max(x,1e-6)])[0]-p([max(x,1e-6)-1e-6])[0]))
-	## normal distribution
-	# mu,sig = scipy.stats.norm.fit(rate)
-	# p = scipy.stats.norm(mu,sig).cdf
-	# pp = lambda x: max(1e-20,float(p([max(x,1e-6)])[0]-p([max(x,1e-6)-1e-6])[0]))
-	#plot_distribution(pp,rate,'Per-molecule sequencing rate','Frequency','Per-molecule sequencing rate')
-	return pp
+    rate = [x[3] / float(x[2] - x[1]) for x in LRs]
+    rate.sort()
+    if len(rate) > 10:
+        rate = rate[int(len(rate) / 10) : int(len(rate) / 10 * 9)]
+    alpha, loc, beta = scipy.stats.gamma.fit(rate)
+    p = scipy.stats.gamma(alpha, loc, beta).cdf
+    pp = lambda x: max(1e-20, float(p([max(x, 1e-6)])[0] - p([max(x, 1e-6) - 1e-6])[0]))
+    ## normal distribution
+    # mu,sig = scipy.stats.norm.fit(rate)
+    # p = scipy.stats.norm(mu,sig).cdf
+    # pp = lambda x: max(1e-20,float(p([max(x,1e-6)])[0]-p([max(x,1e-6)-1e-6])[0]))
+    # plot_distribution(pp,rate,'Per-molecule sequencing rate','Frequency','Per-molecule sequencing rate')
+    return pp
