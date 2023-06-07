93cc86e88cf9784a93e03d318af471e48ea36a50
Pontus Höjer <pontushojer@gmail.com>
2023-06-07 15:00:42 +0200

    update tests to be more robust and add source of testdata for inversions

--- tests/data/make_testdata.sh
+++ tests/data/make_testdata.sh
@@ -17,3 +17,11 @@ samtools view -bh "${bam}" "${region2}" -o HCC1954T_10xGenomics_chr_20_TRA.tmp.b
 samtools cat -o HCC1954T_10xGenomics_chr_12_20_TRA.bam HCC1954T_10xGenomics_chr_12_TRA.tmp.bam HCC1954T_10xGenomics_chr_20_TRA.tmp.bam
 samtools index HCC1954T_10xGenomics_chr_12_20_TRA.bam
 rm HCC1954T_10xGenomics_chr_20_TRA.tmp.bam HCC1954T_10xGenomics_chr_12_TRA.tmp.bam
+
+# INVERSION ON CHROMOSOME 21 (HCC1954T)
+# This is the same as used in the https://github.com/raphael-group/NAIBR/ example
+# hg19: chr21   18759465    18906782
+bam="https://s3-us-west-2.amazonaws.com/10x.files/samples/genome/HCC1954T_WGS_210/HCC1954T_WGS_210_phased_possorted_bam.bam"
+region="chr21:18,709,465-18,956,782"
+samtools view "${bam}" "${region}" -o HCC1954T_10xGenomics_chr21_INV.bam
+samtools index HCC1954T_10xGenomics_chr21_INV.bam
--- tests/test_run.py
+++ tests/test_run.py
@@ -5,11 +5,11 @@ import pytest
 
 from naibr.__main__ import main, run
 from naibr.global_vars import Configs
+from naibr.utils import NovelAdjacency
 
-BAM = Path("tests/data/example_chr21.bam").absolute()
-CANDIDATES = Path("tests/data/example_candidates.bedpe").absolute()
-CANDIDATES_REV = Path("tests/data/example_candidates_reversed.bedpe").absolute()
-BLACKLIST = Path("tests/data/example_blacklist.bed").absolute()
+BAM_INV = Path("tests/data/HCC1954T_10xGenomics_chr21_INV.bam").absolute()
+CANDIDATES_INV = Path("tests/data/HCC1954T_10xGenomics_chr21_INV.candidates.bedpe").absolute()
+CANDIDATES_REV_INV = Path("tests/data/HCC1954T_10xGenomics_chr21_INV.candidates_reversed.bedpe").absolute()
 BAM_TRA = Path("tests/data/HCC1954T_10xGenomics_chr_12_20_TRA.bam").absolute()
 
 
@@ -17,6 +17,15 @@ def same_pairwise_elements(list1, list2):
     assert len(list1) == len(list2)
     for l1, l2 in zip(list1, list2):
         assert l1 == l2
+
+
+def iter_novel_adjacencies(bedpe_reader):
+    for entry in bedpe_reader:
+        # Skip header
+        if entry.startswith("Chr1"):
+            continue
+
+        yield NovelAdjacency.from_naibr(entry)
 
 
 def test_help_return_zero():
@@ -49,7 +58,7 @@ def test_run_from_file(tmp_path):
     configfile = tmp_path / "naibr.config"
     outdir = tmp_path / "results"
     with open(configfile, "w") as f:
-        print(f"bam_file={BAM}\ncandidates={CANDIDATES}\noutdir={outdir}\n", file=f)
+        print(f"bam_file={BAM_INV}\ncandidates={CANDIDATES_INV}\noutdir={outdir}\n", file=f)
 
     errorcode = main([configfile])
     assert errorcode == 0
@@ -60,26 +69,32 @@ def test_run_from_file(tmp_path):
     assert Path(outdir / "NAIBR_SVs.log").exists()
 
 
-def test_candidates(tmp_path):
-    config_file = io.StringIO(f"bam_file={BAM}\ncandidates={CANDIDATES}\noutdir={tmp_path}\nDEBUG=True\n")
+@pytest.mark.parametrize("threads", [1, 2], ids=["singlethread", "multithread"])
+def test_candidates(tmp_path, threads):
+    config_file = io.StringIO(f"bam_file={BAM_INV}\ncandidates={CANDIDATES_INV}\noutdir={tmp_path}\nthreads={threads}\nDEBUG=True\n")
     configs = Configs.from_file(config_file)
 
     exitcode = run(configs)
     assert exitcode == 0
     output = tmp_path / "NAIBR_SVs.bedpe"
-    with open(output) as f:
-        lines = f.readlines()
-
-        assert len(lines) == 2
-        same_pairwise_elements(
-            lines[1].strip().split("\t"),
-            "chr21	18759465	chr21	18906782	28	0	--	1,1	571.967	PASS".split("\t"),
-        )
+    with open(output) as bedpe_reader:
+        nas = list(iter_novel_adjacencies(bedpe_reader))
+        assert len(nas) == 1
+        na = nas[0]
+        assert na.pass_threshold
+        assert na.orient == "--"
+        assert na.chrm1 == na.chrm2 == "chr21"
+        assert na.break1 == 18759465
+        assert na.break2 == 18906782
 
 
 def test_blacklist(tmp_path):
-    assert BLACKLIST.exists()
-    config_file = io.StringIO(f"bam_file={BAM}\nblacklist={BLACKLIST}\noutdir={tmp_path}\n")
+    # Create a blacklist region that overlaps with the candidate region
+    bed = tmp_path / "blacklist.bed"
+    with open(bed, "w") as f:
+        print("chr21\t18000000\t19000000", file=f)
+
+    config_file = io.StringIO(f"bam_file={BAM_INV}\nblacklist={bed}\noutdir={tmp_path}\n")
     configs = Configs.from_file(config_file)
 
     exitcode = run(configs)
@@ -88,84 +103,50 @@ def test_blacklist(tmp_path):
     with open(output) as f:
         lines = f.readlines()
 
-        assert len(lines) == 2
-        same_pairwise_elements(
-            lines[1].strip().split("\t"),
-            "chr21	18842204	chr21	18860085	2	0	++	0,0	19.247	FAIL".split("\t"),
-        )
+        # Only header line should be present
+        assert len(lines) == 1
 
 
 def test_candidates_reversed(tmp_path):
-    config_file = io.StringIO(f"bam_file={BAM}\ncandidates={CANDIDATES_REV}\noutdir={tmp_path}\n")
+    config_file = io.StringIO(f"bam_file={BAM_INV}\ncandidates={CANDIDATES_REV_INV}\noutdir={tmp_path}\n")
     configs = Configs.from_file(config_file)
 
     exitcode = run(configs)
     assert exitcode == 0
     output = tmp_path / "NAIBR_SVs.bedpe"
-    with open(output) as f:
-        lines = f.readlines()
+    with open(output) as bedpe_reader:
+        nas = list(iter_novel_adjacencies(bedpe_reader))
+        assert len(nas) == 1
+        na = nas[0]
+        assert na.pass_threshold
+        assert na.orient == "--"
+        assert na.chrm1 == na.chrm2 == "chr21"
+        assert na.break1 == 18759465
+        assert na.break2 == 18906782
 
-        assert len(lines) == 2
-        same_pairwise_elements(
-            lines[1].strip().split("\t"),
-            "chr21	18759465	chr21	18906782	28	0	--	1,1	571.967	PASS".split("\t"),
-        )
 
-
-def test_candidates_parallel(tmp_path):
-    config_file = io.StringIO(f"bam_file={BAM}\ncandidates={CANDIDATES}\noutdir={tmp_path}\nthreads=2\n")
+@pytest.mark.parametrize("threads", [1, 2], ids=["singlethread", "multithread"])
+def test_call_inversion(tmp_path, threads):
+    config_file = io.StringIO(f"bam_file={BAM_INV}\noutdir={tmp_path}\nthreads={threads}\nDEBUG=True\n")
     configs = Configs.from_file(config_file)
 
     exitcode = run(configs)
     assert exitcode == 0
     output = tmp_path / "NAIBR_SVs.bedpe"
-    with open(output) as f:
-        lines = f.readlines()
-
-        assert len(lines) == 2
-        same_pairwise_elements(
-            lines[1].strip().split("\t"),
-            "chr21	18759465	chr21	18906782	28	0	--	1,1	571.967	PASS".split("\t"),
-        )
-
-
-def test_novel(tmp_path):
-    config_file = io.StringIO(f"bam_file={BAM}\noutdir={tmp_path}\nDEBUG=True\n")
-    configs = Configs.from_file(config_file)
-
-    exitcode = run(configs)
-    assert exitcode == 0
-    output = tmp_path / "NAIBR_SVs.bedpe"
-    with open(output) as f:
-        lines = f.readlines()
-
-        assert len(lines) == 2
-        same_pairwise_elements(
-            lines[1].strip().split("\t"),
-            "chr21	18842204	chr21	18860085	2	0	++	0,0	19.247	FAIL".split("\t"),
-        )
-
-
-def test_novel_parallel(tmp_path):
-    config_file = io.StringIO(f"bam_file={BAM}\noutdir={tmp_path}\nthreads=2\n")
-    configs = Configs.from_file(config_file)
-
-    exitcode = run(configs)
-    assert exitcode == 0
-    output = tmp_path / "NAIBR_SVs.bedpe"
-    with open(output) as f:
-        lines = f.readlines()
-
-        assert len(lines) == 2
-        same_pairwise_elements(
-            lines[1].strip().split("\t"),
-            "chr21	18842204	chr21	18860085	2	0	++	0,0	19.247	FAIL".split("\t"),
-        )
+    with open(output) as bedpe_reader:
+        nas = list(iter_novel_adjacencies(bedpe_reader))
+        assert len(nas) > 0
+        nas_pass = [na for na in nas if na.pass_threshold]
+        assert len(nas_pass) == 1
+        assert nas_pass[0].orient == "--"
+        assert nas_pass[0].chrm1 == nas_pass[0].chrm2 == "chr21"
+        assert abs(nas_pass[0].break1 - 18759465) < 500
+        assert abs(nas_pass[0].break2 - 18906782) < 500
 
 
 def test_consistent(tmp_path):
     # Run without candidates
-    config_file = io.StringIO(f"bam_file={BAM}\noutdir={tmp_path / 'novel'}\nthreads=2\n")
+    config_file = io.StringIO(f"bam_file={BAM_INV}\noutdir={tmp_path / 'novel'}\nthreads=2\n")
     configs = Configs.from_file(config_file)
 
     exitcode = run(configs)
@@ -181,7 +162,7 @@ def test_consistent(tmp_path):
 
     # Run with generated candidates
     rerun_config_file = io.StringIO(
-        f"bam_file={BAM}\ncandidates={new_candidates}\noutdir={tmp_path / 'rerun'}\nthreads=2\n"
+        f"bam_file={BAM_INV}\ncandidates={new_candidates}\noutdir={tmp_path / 'rerun'}\nthreads=2\n"
     )
     rerun_configs = Configs.from_file(rerun_config_file)
 
