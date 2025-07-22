012902e9be1c82c6e00f969f7c58814decd560c6
Pontus Höjer <pontus.hojer@scilifelab.se>
2025-07-22 11:26:59 +0200

    rephrase bam specs

--- src/naibr/__main__.py
+++ src/naibr/__main__.py
@@ -6,7 +6,8 @@ Usage: naibr input.config
 Running requires a config file with "=" separated parameters and values.
 
 required:
-    bam_file   - str  - Path to BAM file with (phased) linked paired-end reads, i.e. has BX and possibly HP tags.
+    bam_file   - str  - Path to BAM file with barcode-linked (BX tag) paired-end reads. To get haplotyped 
+                        SVs provided phased read (HP tag). 
 
 options:
     outdir     - str  - Path to output directory. Default: CWD
